b807a49614b79c3b9393b7b840fdb3c9b437bdeb
royki <kearan.rayofsun@gmail.com>
2026-01-28 05:31:44 -0500

    debug: insufficent balance

--- packages/ui/src/hooks/useGetBalance.tsx
+++ packages/ui/src/hooks/useGetBalance.tsx
@@ -21,10 +21,11 @@ export const useGetBalance = ({
   const { existentialDeposit } = useGetED({ withPplApi })
 
   useEffect(() => {
-    if (!api || !address || !existentialDeposit) return
+    if (!api || !address || existentialDeposit === undefined) return
 
-    const unsub = api.query.System.Account.watchValue(address, 'best').subscribe(
-      ({ data: { free, frozen, reserved } }) => {
+    const unsub = api.query.System.Account.watchValue(address, 'best').subscribe({
+      next: (accountInfo) => {
+        const { free, frozen, reserved } = accountInfo?.data || { free: 0n, frozen: 0n, reserved: 0n }
         const res = free - bigIntMax(frozen - reserved, existentialDeposit)
         const transferable = res < 0n ? 0n : res
         // #region agent log
@@ -46,8 +47,21 @@ export const useGetBalance = ({
             tokenSymbol: chainInfo?.tokenSymbol
           })
         )
+      },
+      error: (err) => {
+        // #region agent log
+        console.error('[DEBUG useGetBalance] Subscription error:', err);
+        // #endregion
+        // Set balance to 0 on error
+        setBalance(0n)
+        setFormattedBalance(
+          formatBigIntBalance(0n, chainInfo?.tokenDecimals, {
+            numberAfterComma,
+            tokenSymbol: chainInfo?.tokenSymbol
+          })
+        )
       }
-    )
+    })
 
     return () => unsub && unsub.unsubscribe()
   }, [address, api, chainInfo, existentialDeposit, numberAfterComma])
